998d89ae2a3db6354503d984e6faef49780add39
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-12 16:37:39 +0530

    Static subcomponents for task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react"
+import React from "react"
 import {hot} from "react-hot-loader"
 import "./App.css"
 
-class App extends Component{
+class App extends React.Component{
   render() {
     return(
       <div className="App">
@@ -11,5 +11,41 @@ class App extends Component{
     )
   }
 }
-  
+
+class EditField extends React.Component{
+  constructor (props) {
+    super(props)
+    this.state = {value: props.value, enabled: false}
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange () {
+    this.setState({value: event.target.value})
+  }
+  render () {
+    <input type='text' className='' disabled='' ></input>
+  }
+}
+
+function EditBtn (props) {
+  if (props.editNameOn) {
+    return <button className='acceptNameEditBtn'></button>
+  } return <button className='editBtn'></button>
+} 
+
+function DescribeBtn (props) {
+  if (props.editDescOn) {
+    return <button className='acceptDescEditBtn'></button>
+  } return <button className='describeBtn'></button>
+}
+
+function CompleteBtn (props) {
+  if (props.completed) {
+    return <button className='incompleteBtn'></button>
+  } return <button className='completeBtn'></button>
+}
+
+function DeleteBtn (props) {
+  return <button className='deleteBtn'></button>
+}
+
 export default hot(module)(App)
