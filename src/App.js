44f196949449d88e6708d4ea32963b3fe70bcfca
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-15 09:02:39 +0530

    Remodelled data flow in React

--- src/App.js
+++ src/App.js
@@ -2,50 +2,74 @@ import React from "react"
 import {hot} from "react-hot-loader"
 import "./App.css"
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   render() {
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1>Carpe Diem</h1>
+        
+        <div id="inputDivider" class="divider"></div>
+
+        <div id="completeDivider" class="divider"></div>
+        <h2>Completed</h2>
+
       </div>
     )
   }
 }
 
-class EditField extends React.Component{
+class Task extends React.Component {
+  constructor (props) {
+    super(props) // contains name, desc, completed
+    this.state = { editNameOn: false, editDescOn: false } 
+  }
+  editOn () {
+    this.state.editNameOn = true
+  }
+  descOn () {
+    this.state.editDescOn = true
+  }
+  render() { // set id?
+    <div className='task'>
+    <EditField class='taskname' value={this.props.name} btnOnClass='acceptNameEditBtn' btnOffClass='editBtn'> submitFunc={}</EditField>
+    <EditField class='taskdesc' value={this.props.desc} btnOnClass='acceptDescEditBtn' btnOffClass='describeBtn'> submitFunc={}</EditField>
+    <ToggleBtn condition={this.props.completed} onClass='incompleteBtn' offClass='completeBtn' clickAction={}></ToggleBtn>
+    <button className='deleteBtn' onClick={}></button>
+    </div>
+  }
+}
+
+class EditField extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {value: props.value, enabled: false}
+    this.state = {value: props.value, editOn: false}
     this.handleChange = this.handleChange.bind(this)
   }
-  handleChange () {
+  handleChange (event) {
     this.setState({value: event.target.value})
   }
-  render () {
-    <input type='text' className='' disabled='' ></input>
+  handleClick (event) {
+    if (this.state.editOn) {
+      //put request of some props type using this.state.value
+    }
+    this.state.editOn = !this.state.editOn
+  }
+  render() {
+    <div>
+    <input type='text' className={this.props.class} disabled={!this.state.editOn} ></input>
+    <ToggleBtn condition={this.state.editOn} onClass={this.props.btnOnClass} offClass={this.props.btnOffClass} clickAction={this.handleClick}></ToggleBtn>
+    </div>
   }
 }
 
-function EditBtn (props) {
-  if (props.editNameOn) {
-    return <button className='acceptNameEditBtn'></button>
-  } return <button className='editBtn'></button>
-} 
-
-function DescribeBtn (props) {
-  if (props.editDescOn) {
-    return <button className='acceptDescEditBtn'></button>
-  } return <button className='describeBtn'></button>
-}
-
-function CompleteBtn (props) {
-  if (props.completed) {
-    return <button className='incompleteBtn'></button>
-  } return <button className='completeBtn'></button>
-}
-
-function DeleteBtn (props) {
-  return <button className='deleteBtn'></button>
+function ToggleBtn (props) {
+  if (props.condition) {
+    return <button className={this.props.onClass} onClick={this.props.clickAction}></button>
+  } return <button className={this.props.offClass} onClick={this.props.clickAction}></button>
 }
 
 export default hot(module)(App)
