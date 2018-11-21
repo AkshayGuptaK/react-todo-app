b58b28d1e5d163d17e2e33f905edf747e6ab2e25
Akshay Gupta <akshaygrkesh@gmail.com>
2018-11-21 09:53:02 +0530

    New routes for multilist

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+var router = express.Router()
+const controllers = require('/controllers.js')
+
+/* GET all data */
+router.get('/all', controllers.getAllData)
+
+/* GET all task data of a list */
+router.get('/list/:listId', controllers.getAllTasks)
+
+/* POST new list */
+router.post('/list/:name', controllers.addList)
+
+/* PUT list name edit */
+router.put('/list/:listId/:name', controllers.editList)
+
+/* DELETE list */
+router.delete('/list/:listId', controllers.deleteList)
+
+/* POST new task */
+router.post('/task/:listId/:name/:desc', controllers.addTask)
+
+/* PUT task edit */
+router.put('/task/:listId/:taskId/:field/:value', controllers.editTask)
+
+/* DELETE a task */
+router.delete('/task/:listId/:taskId', controllers.deleteTask)
+
+module.exports = router
+
